547291e6ba9a57d05e100a463c3c5f712e5e02fa
Dawie <dvanlill@sun.ac.za>
2020-10-26 10:27:13 +0200

    Root-finding algorithms

---
+++ _research/wip/root-finding/newton/fnc_newton.jl
@@ -0,0 +1,12 @@
+
+# Newtons method for scalar root finding -- univariate case from Fundamentals of Numerical Computation. 
+
+# In Julia the docstring is found above the function, not the same as in python. 
+
+"""
+newton(f, dfdx, x1)
+
+Use Newton's method to find a root of `f` starting from `x1`, where `dfdx` is the derivative of `f`. This returns a vector of root estimates.
+"""
+function newton(f, dfdx, x1)
+end

8d48c1d191ce3c49b5543f833de36a2e4eeec108
Dawie <dvanlill@sun.ac.za>
2020-10-27 11:26:42 +0200

    Root-finding files added

--- _research/wip/root-finding/bisection/bisection.jl
+++
@@ -1,3 +0,0 @@
-
-# This is a script for the bisection algorithm. The basic idea is taken from Wheeler et al. in their book on the Algorithms for optimisation
-
---
+++ _research/wip/root-finding/univariate/bisection.jl
@@ -0,0 +1,3 @@
+
+# This is a script for the bisection algorithm. The basic idea is taken from Wheeler et al. in their book on the Algorithms for Optimisation
+

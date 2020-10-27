65e6ab386df1d116762c069306f25761ec293af7
Dawie <dvanlill@sun.ac.za>
2020-10-27 11:40:21 +0200

    Miranda / Fackler bisection included

--- _research/wip/root-finding/univariate/bisection.jl
+++ _research/wip/root-finding/univariate/bisection.jl
@@ -1,3 +1,48 @@
 
-# This is a script for the bisection algorithm. The basic idea is taken from Wheeler et al. in their book on the Algorithms for Optimisation
+#=
+Bisection algorithm
+
+@author: Dawie van Lill <dvanlill@sun.ac.za>
+
+@date: 2020-10-27
+
+References
+----------
+Wheeler - Algorithms for Optimisation p49
+Miranda and Fackler -- Applied Computational Economics and Finance
+Driscoll -- Fundamentals of Numerical Computation
+
+=#
+
+using Parameters
+
+@with_kw struct params
+    tol::Float64 = 1e-8
+end
+
+f = x -> x^3 - 2;
+p = params()
+
+function fackler_bisection(f, a, b)
+    @unpack tol = p
+
+    s = sign(f(a))
+    x = (a + b) / 2
+    d = (b - a) / 2
+
+    while d > tol
+        d = d/2
+
+        if s == sign(f(x))
+            x = x + d
+        else
+            x = x - d
+        end
+    end
+    return x
+end
+
+
+
+
 

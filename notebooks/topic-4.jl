8fb171204e609ddb9fc37726eb2b65febe39819b
Dawie <dvanlill@sun.ac.za>
2021-05-06 09:27:33 +0200

    Start with root finding

--- notebooks/topic-4.jl
+++ notebooks/topic-4.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.1
+# v0.14.5
 
 using Markdown
 using InteractiveUtils
@@ -29,6 +29,20 @@ end
 # ╔═╡ 1f4407d0-9b73-11eb-0e91-cd0de83535aa
 md" # Optimisation "
 
+# ╔═╡ 16478345-300b-460b-8198-faccaf7740e9
+md" We will probably need about two to three sessions to cover the basic optimisation ideas that are relevant for economics. Optimisation is an incredibly important topic in economics, and many related disciplines, so it is worthwhile to spend some time exploring the general themes.
+
+Julia has a well-developed ecosystem for optimisation routines and can comfortably used to meet all your optimisation needs. We will speak to the packages relevant for optimisation later in this session. Our first topic is going to be root finding, then we will move to unconstrained optimisation and end with constrained optimisation."
+
+# ╔═╡ 0170f06a-79ba-4ba3-bfe0-db1e63bc7b74
+md" ### Root finding"
+
+# ╔═╡ 46095078-d68c-4570-b2b5-b8aad3656186
+
+
 # ╔═╡ Cell order:
 # ╟─f4226cfe-ee06-4c72-9615-fc4aedfd045c
 # ╟─1f4407d0-9b73-11eb-0e91-cd0de83535aa
+# ╟─16478345-300b-460b-8198-faccaf7740e9
+# ╟─0170f06a-79ba-4ba3-bfe0-db1e63bc7b74
+# ╠═46095078-d68c-4570-b2b5-b8aad3656186
---
+++ scripts/iterative.jl
@@ -0,0 +1,22 @@
+
+# Live coding session
+
+using LinearAlgebra
+
+A = I(2)
+cond(A)
+
+ϵ = 1E-6
+
+A2 = [1.0 0.0
+     1.0 ϵ]
+
+cond(A2)     
+
+inv(A2)
+
+det(A2) # poor conditioning
+
+# Gauss-Jacobi, Gauss-Seidel, Conjugate gradient (newest) 
+
+

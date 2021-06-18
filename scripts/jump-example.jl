1895fa1cde4161b2bc9c8d30dc6e575aceec402a
Dawie <dvanlill@sun.ac.za>
2021-06-18 15:51:59 +0200

    JuMP examples

---
+++ scripts/jump-example.jl
@@ -0,0 +1,27 @@
+
+
+using JuMP
+using GLPK
+
+model = Model(with_optimizer(GLPK.Optimizer)) # Specify the optimizer
+@variable(model, 0 <= x <= 2) # Provide range for x variable 
+@variable(model, 0 <= y <= 30) # Provide range for y variable 
+
+# next, we set an objective function
+@objective(model, Max, 5x + 3y)
+
+# maybe add a constraint called "con"?
+@constraint(model, con, 1x + 5y <= 3)
+
+JuMP.optimize!(model)
+
+# look at status
+termination_status(model)
+
+# we query objective value and solutions
+@show objective_value(model)
+@show value(x)
+@show value(y)
+
+# as well as the value of the dual variable on the constraint (Lagrange multiplier)
+@show dual(con);
---
+++ scripts/jump-rosenbrock.jl
@@ -0,0 +1,45 @@
+
+
+# JuMP: nonlinear Rosenbrock Example
+# Instead of hand-coding first and second derivatives, you only have to give `JuMP` expressions for objective and constraints.
+# Here is an example.
+
+using Ipopt
+
+let
+
+    m₁ = Model(with_optimizer(Ipopt.Optimizer))
+
+    @variable(m₁, x₁)
+    @variable(m₁, y₁)
+
+    @NLobjective(m₁, Min, (1-x₁)^2 + 100(y₁-x₁^2)^2)
+
+    JuMP.optimize!(m₁)
+    @show value(x₁)
+    @show value(y₁)
+    @show termination_status(m₁)
+
+end
+
+# not bad, right?
+# adding the constraint from before:
+
+let
+    
+    m₂ = Model(with_optimizer(Ipopt.Optimizer))
+
+    @variable(m₂, x₂)
+    @variable(m₂, y₂)
+
+    @NLobjective(m₂, Min, (1-x₂)^2 + 100(y₂-x₂^2)^2)
+
+
+    @NLconstraint(m₂, x₂^2 + y₂^2 <= 0.8)
+
+    JuMP.optimize!(m₂)
+    @show value(x₂)
+    @show value(y₂)
+    @show termination_status(m₂)
+
+end

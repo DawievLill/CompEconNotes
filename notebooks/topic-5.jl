d55a0907c0e129cb49d100790d86df2d06b8e0a7
Dawie <dvanlill@sun.ac.za>
2021-06-29 11:03:02 +0200

    FA introduction

--- notebooks/topic-5.jl
+++ notebooks/topic-5.jl
@@ -40,7 +40,11 @@ html"""
 # ╔═╡ 0ac4cb74-c027-4677-bd45-672f19035ce4
 md" # Function approximation "
 
+# ╔═╡ 25b6e79e-37dc-4a67-8b56-fbaf9da41713
+md" We will start with a discussion on basis functions. This will lead us into the general idea behind approximation. Next we focus on orthogonal polynomials, which is followed by a look at splines. We will also introduce some Julia packages that allow for interpolation. "
+
 # ╔═╡ Cell order:
 # ╟─eeca0160-d37e-11eb-05f4-bd7469319dad
 # ╟─99e0b1b9-d032-41d6-8a1d-742050d5ddeb
 # ╟─0ac4cb74-c027-4677-bd45-672f19035ce4
+# ╟─25b6e79e-37dc-4a67-8b56-fbaf9da41713

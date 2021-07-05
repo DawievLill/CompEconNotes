dfac4a9f1c814142a5c5ea54086c2b39d3b6b071
Dawie <dvanlill@sun.ac.za>
2021-07-05 11:51:55 +0200

    Finite element methods

--- notebooks/topic-5.jl
+++ notebooks/topic-5.jl
@@ -8,24 +8,12 @@ using InteractiveUtils
 begin
 	import Pkg
 	Pkg.activate(mktempdir())
-	Pkg.add([
-			Pkg.PackageSpec(name="Images"), 
-			Pkg.PackageSpec(name="ImageMagick"), 
-			Pkg.PackageSpec(name="PlutoUI"), 
-			Pkg.PackageSpec(name="HypertextLiteral"),
-			Pkg.PackageSpec(name="Random"), 
-			Pkg.PackageSpec(name="BenchmarkTools"), 
-			Pkg.PackageSpec(name="Plots"), 
-			Pkg.PackageSpec(name="LaTeXStrings")
-			])
-	using Random
-	using BenchmarkTools
-	using Images
-	using PlutoUI
-	using HypertextLiteral
-	using LinearAlgebra
-	using Plots
-	using LaTeXStrings
+end
+
+# ╔═╡ dad03d6a-e941-4980-b780-826e417e6311
+begin
+		Pkg.add([Pkg.PackageSpec(name="Plots")])
+		using Plots
 end
 
 # ╔═╡ 99e0b1b9-d032-41d6-8a1d-742050d5ddeb
@@ -69,10 +57,12 @@ These $\beta$ values are the coefficients of the linear combination. The set of 
 The following figure shows the span of $A=\left\{a_{1}, a_{2}\right\}$ in $\mathbb{R}^{3}$. It provides a 2 dimensional plane passing through these two points and the origin. 
 "
 
+# ╔═╡ 8313e1ee-7c01-4eda-8d4b-888f04de5c90
+plotly()
+
 # ╔═╡ b6f096f4-9857-4d66-a62d-8dd818c4d9a2
 begin
-	
-	plotly()
+
 	# fixed linear function, to generate a plane
 	f(x, y) = 0.1x + 0.1y
 	
@@ -103,10 +93,26 @@ Similar to the way in which every vector in a vector space can be represented by
 
 
 # ╔═╡ 55cfd73a-7015-4936-b229-ddcc488d48de
-md" ## Interpolation basics"
+md" ## Interpolation based methods "
 
 # ╔═╡ 59dffcb5-b978-495e-a25e-22a57ae84882
-md" As mentioned before, there are three broad approaches to approximation. This section links interpolation and regression to the concept of a basis function. We will deal with local approximation through Taylor series expansion toward the end of the session.
+md" Interpolation is a form of function approximation in which the interpolant and the true function must agree. In other words, they must have same value at a finite number of points. Additional restrictions can also be imposed on the interpolant, such as constraints on monotonicity, convexity and smoothness. In general, interpolation is any method that takes information at a finite set of points and finds a function that satisfies that information.
+
+There are are two primary approaches to interpolation. The approaches are **finite element methods** and **spectral methods**. We will talk about finite element methods first and then move on to spectral methods toward the end of the session. "
+
+# ╔═╡ ef709d7b-107b-43bb-9026-c7af1efacfdc
+md" ### Interpolation basics "
+
+# ╔═╡ 5e55cf7d-a076-45e1-8c54-c561be82857d
+md" ### Finite element methods "
+
+# ╔═╡ 95125a71-1691-4009-acb4-5db30cd6b69f
+md" ### Spectral methods "
+
+# ╔═╡ ef9fb41f-b815-4e77-8913-82ee14fc3d8c
+md" This section links interpolation and regression to the concept of a basis function. We will deal with local approximation through Taylor series expansion toward the end of the session. This section draws from Chapter 6 of Judd and Chapter 6 of Miranda and Fackler. 
+
+
 
 The space of continuous functions is spanned by monomials, $x^{n}$, $n = 0, 1, 2, \ldots$, and one can use the monomials as a basis for the space of continuous functions. However, we have to ask ourselves whether this is a good basis. Normally a good basis for a vector space also has some orthogonality properties. This leads us into the discussion of constructing orthogonal polynomials as bases for our function space. In order to think about orthogonality one needs the concept of an inner product in the vector space. 
 
@@ -114,21 +120,27 @@ Given that $F$ is a space of continuous real valued functions, let us define a i
 
 $<g, h>=\int_{\mathbf{x}} g(x) h(x) w(x) d x$
 
-where $g, h, w \in F$ and $w$ is a weighting function. The pair $\{F,<.,.>\}$ form an inner-product vetor space. "
+where $g, h, w \in F$ and $w$ is a weighting function. The pair $\{F,<.,.>\}$ form an inner-product vetor space."
 
-# ╔═╡ 95125a71-1691-4009-acb4-5db30cd6b69f
-
+# ╔═╡ 0a9c73e1-68c1-4c17-bd48-98673b44d332
+md" ## Local approximations " 
 
 # ╔═╡ Cell order:
 # ╟─eeca0160-d37e-11eb-05f4-bd7469319dad
+# ╟─dad03d6a-e941-4980-b780-826e417e6311
 # ╟─99e0b1b9-d032-41d6-8a1d-742050d5ddeb
 # ╟─0ac4cb74-c027-4677-bd45-672f19035ce4
 # ╟─25b6e79e-37dc-4a67-8b56-fbaf9da41713
 # ╟─a4f24d72-fb51-47cc-827d-c9f3cb367998
 # ╟─9ffc49e0-056b-4029-87ce-2fee216277a5
 # ╟─31bc8646-bf07-4a22-9030-196af852251c
-# ╟─b6f096f4-9857-4d66-a62d-8dd818c4d9a2
+# ╠═8313e1ee-7c01-4eda-8d4b-888f04de5c90
+# ╠═b6f096f4-9857-4d66-a62d-8dd818c4d9a2
 # ╟─7fc20a57-f784-4336-a71f-bb2b0bbdc777
 # ╟─55cfd73a-7015-4936-b229-ddcc488d48de
 # ╟─59dffcb5-b978-495e-a25e-22a57ae84882
-# ╠═95125a71-1691-4009-acb4-5db30cd6b69f
+# ╟─ef709d7b-107b-43bb-9026-c7af1efacfdc
+# ╟─5e55cf7d-a076-45e1-8c54-c561be82857d
+# ╟─95125a71-1691-4009-acb4-5db30cd6b69f
+# ╟─ef9fb41f-b815-4e77-8913-82ee14fc3d8c
+# ╟─0a9c73e1-68c1-4c17-bd48-98673b44d332

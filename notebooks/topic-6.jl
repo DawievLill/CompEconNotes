5e4200fe18e31f1d4fe18cfb7b94d8bd7ff6559e
Dawie <dvanlill@sun.ac.za>
2021-07-12 22:08:52 +0200

    Numerical integration

---
+++ notebooks/topic-6.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ cdb726be-e34c-11eb-17ba-010997657721
+md" # Numerical Integration "
+
+# ╔═╡ Cell order:
+# ╟─cdb726be-e34c-11eb-17ba-010997657721
